11d88cdbc8e21473d4bf87a1e355ed8656ad4dcc
diskree <sergei.grushin@imem.app>
2023-05-01 15:06:03 +0400

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostBinding, NgZone, ViewChild } from '@angular/core';
+import { CdkTextareaAutosize } from '@angular/cdk/text-field';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ClipboardService } from 'ngx-clipboard';
+import { take } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(
+    private _ngZone: NgZone,
+    private clipboardService: ClipboardService,
+    private snackBar: MatSnackBar,
+    private overlay: OverlayContainer
+  ) { }
+
   title = 'ChangelogGenerator';
+  toggleControl = new FormControl(false);
+  checked = false;
+
+  @ViewChild('autosize') autosize!: CdkTextareaAutosize;
+  @ViewChild('ru_changelog') ruChangelogArea!: ElementRef;
+  @ViewChild('en_changelog') enChangelogArea!: ElementRef;
+  @ViewChild('sticker_pack') stickerPackArea!: ElementRef;
+  @ViewChild('sticker_number') stickerNumberArea!: ElementRef;
+  @HostBinding('class') className = '';
+
+  triggerResize() {
+    this._ngZone.onStable.pipe(take(1)).subscribe(() => this.autosize.resizeToFitContent(true));
+  }
+
+  copyToClipboard() {
+    const data = {
+      ru_changelog: this.ruChangelogArea.nativeElement.value,
+      en_changelog: this.enChangelogArea.nativeElement.value,
+      sticker_pack: this.stickerPackArea.nativeElement.value,
+      sticker_number: this.stickerNumberArea.nativeElement.value,
+    };
+    const jsonString = JSON.stringify(data, null, 2);
+    this.clipboardService.copy(jsonString);
+    this.openSnackBar('JSON скопирован в буфер обмена', 'OK')
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 1500,
+    });
+  }
+
+  toggleTheme() {
+    this.checked = !this.checked;
+    this.className = this.checked ? 'dayMode' : '';
+    if (this.checked) {
+      this.overlay.getContainerElement().classList.add('dayMode');
+    } else {
+      this.overlay.getContainerElement().classList.remove('dayMode');
+    }
+  }
+
+  openGithub() {
+    window.open('https://github.com/diskree/ChangelogGenerator', '_blank');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
